53a322fce4ea2cd917e70fc2c2e034831a86586e
Gato de Gravata <knowhowfelipe@gmail.com>
2026-04-25 14:38:37 -0300

    Update automatico: 25/04/2026 14:38:29,45

---
+++ subir_pro_git.bat
@@ -0,0 +1,23 @@
+@echo off
+echo Iniciando o Vulp-Odds para o GitHub...
+
+:: Navega ate a pasta do projeto (ajuste se necessario)
+cd /d "C:\proj\apostas\api-oddsapi.io"
+
+:: Pega a data e hora atual para o commit
+set dt=%date% %time%
+
+echo Preparando arquivos...
+git add .
+
+echo Criando commit com data: %dt%
+git commit -m "Update automatico: %dt%"
+
+echo Subindo para o GitHub (Main)...
+git push origin main
+
+echo.
+echo Tudo pronto! Projeto atualizado no GitHub.
+echo Finalizado em: %dt%
+echo.
+pause
